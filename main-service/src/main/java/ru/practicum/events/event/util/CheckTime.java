55be3704a108d3eb3b45a1dd6afbcf43951f91b2
Oksana <ashkeeper@mail.ru>
2023-03-07 22:27:55 +0300

    style: выровнила код

--- main-service/src/main/java/ru/practicum/events/event/util/CheckTime.java
+++ main-service/src/main/java/ru/practicum/events/event/util/CheckTime.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 public class CheckTime {
     public static void checkTemporaryValidation(LocalDateTime checked, LocalDateTime original, Integer difference) {
-        if(checked.isBefore(original.minusHours(difference))) {
+        if (checked.isBefore(original.minusHours(difference))) {
             throw new ConflictException("Нельзя изменить событие. Дата начала изменяемого события должна быть не " +
                     "ранее чем за час от даты публикации");
         }
