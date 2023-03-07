50439136713b70d8fb403e74fe819cfd2392a824
Oksana <ashkeeper@mail.ru>
2023-03-07 22:08:36 +0300

    fix: настройка

--- stats-service/stats-client/src/main/java/ru/practicum/explore/BaseClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/explore/BaseClient.java
@@ -20,7 +20,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 public abstract class BaseClient {
     WebClient client;
 
-    public BaseClient(String uri){
+    public BaseClient(String uri) {
         client = WebClient.create(uri);
     }
 
